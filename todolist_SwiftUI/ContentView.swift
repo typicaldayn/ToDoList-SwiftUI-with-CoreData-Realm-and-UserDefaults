30eff98a88a4bae9ae2cefc97ab46c75d5cab330
stasbezhan <krig.ua1968@gmail.com>
2022-09-16 09:26:11 +0300

    Initial Commit

---
+++ todolist_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  todolist_SwiftUI
+//
+//  Created by Stas Bezhan on 16.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todolist_SwiftUI/todolist_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolist_SwiftUIApp.swift
+//  todolist_SwiftUI
+//
+//  Created by Stas Bezhan on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct todolist_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
